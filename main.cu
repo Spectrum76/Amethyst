1b7ad83f20c5bacead650f43dfdcffec2a126bae
Rayvant <rayvant1070@gmail.com>
2023-11-25 15:28:12 +0530

    Surface Normals.

--- main.cu
+++ main.cu
@@ -9,27 +9,35 @@
 #include "vec3.h"
 #include "ray.h"
 
-__device__ bool hit_sphere(const vec3& center, float radius, const ray& r)
+__device__ float hit_sphere(const vec3& center, float radius, const ray& r)
 {
 	vec3 oc = r.origin() - center;
-	auto a = dot(r.direction(), r.direction());
-	auto b = 2.0f * dot(oc, r.direction());
-	auto c = dot(oc, oc) - radius * radius;
-	auto discriminant = b * b - 4 * a * c;
-	return (discriminant >= 0);
+	auto a = r.direction().length_squared();
+	auto half_b = dot(oc, r.direction());
+	auto c = oc.length_squared() - radius * radius;
+	auto discriminant = half_b * half_b - a * c;
+
+	if (discriminant < 0)
+		return -1.0f;
+	else
+		return (-half_b - sqrt(discriminant)) / a;
 }
 
 __device__ vec3 ray_color(const ray& r)
 {
-	if (hit_sphere(vec3(0, 0, -1), 0.5f, r))
-		return vec3(1, 0, 0);
+	auto t = hit_sphere(vec3(0, 0, -1), 0.5f, r);
+	if (t > 0.0)
+	{
+		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+		return 0.5f * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+	}
 
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5f * (unit_direction.y() + 1.0f);
 	return (1.0f - a) * vec3(1.0f, 1.0f, 1.0f) + a * vec3(0.5f, 0.7f, 1.0f);
 }
 
-__global__ void render(vec3* fb, int max_x, int max_y,
+__global__ void render(uint8_t* fb, int max_x, int max_y,
 	vec3 camera_center, vec3 pixel_delta_u, vec3 pixel_delta_v, vec3 pixel00_loc)
 {
 	int i = threadIdx.x + blockIdx.x * blockDim.x;
@@ -41,9 +49,11 @@ __global__ void render(vec3* fb, int max_x, int max_y,
 
 	vec3 pixel_color = ray_color(r);
 
-	int pixel_index = j * max_x + i;
+	int pixel_index = j * max_x * 3 + i * 3;
 
-	fb[pixel_index] = pixel_color;
+	fb[pixel_index + 0] = static_cast<uint8_t>(255.999 * pixel_color.r());
+	fb[pixel_index + 1] = static_cast<uint8_t>(255.999 * pixel_color.g());
+	fb[pixel_index + 2] = static_cast<uint8_t>(255.999 * pixel_color.b());
 }
 
 int main()
@@ -72,9 +82,9 @@ int main()
 	auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
 	int num_pixels = image_width * image_height;
-	size_t fb_size = num_pixels * sizeof(vec3);
+	size_t fb_size = 3 * num_pixels * sizeof(uint8_t);
 
-	vec3* fb;
+	uint8_t* fb;
 	cudaMallocManaged((void**)&fb, fb_size);
 
 	auto start = std::chrono::high_resolution_clock::now();
@@ -82,7 +92,7 @@ int main()
 	dim3 blocks(image_width / thread_x, image_height / thread_y);
 	dim3 threads(thread_x, thread_y);
 
-	render<<<blocks, threads>>>(fb, image_width, image_height, camera_center, pixel_delta_u, pixel_delta_v, pixel00_loc);
+	render <<<blocks, threads>>> (fb, image_width, image_height, camera_center, pixel_delta_u, pixel_delta_v, pixel00_loc);
 
 	cudaGetLastError();
 	cudaDeviceSynchronize();
@@ -91,25 +101,7 @@ int main()
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 	std::cout << "\nTook " << duration.count() << " milliseconds" << std::endl;
 
-	std::vector<uint8_t> image_data;
-
-	for (int j = 0; j < image_height; ++j)
-	{
-		for (int i = 0; i < image_width; ++i)
-		{
-			size_t pixel_index = j * image_width + i;
-
-			int ir = static_cast<int>(255.999 * fb[pixel_index].r());
-			int ig = static_cast<int>(255.999 * fb[pixel_index].g());
-			int ib = static_cast<int>(255.999 * fb[pixel_index].b());
-
-			image_data.push_back(ir);
-			image_data.push_back(ig);
-			image_data.push_back(ib);
-		}
-	}
-
-	stbi_write_png("Amethyst.png", image_width, image_height, 3, image_data.data(), image_width * 3);
+	stbi_write_png("Amethyst.png", image_width, image_height, 3, fb, image_width * 3);
 
 	cudaFree(fb);
 }
